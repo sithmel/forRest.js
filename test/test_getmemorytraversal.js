2580bf70f94cba0de3b1c7e114893338bd45f2ee
Maurizio Lupo <maurizio.lupo@gmail.com>
2013-05-10 12:59:12 +0200

    added tests on memory traversal

--- test/test_getmemorytraversal.js
+++ test/test_getmemorytraversal.js
@@ -6,7 +6,8 @@
 
 var should = require('should'),
     getMemoryTraversal = require('../lib/getmemorytraversal'),
-    Traversal = getMemoryTraversal({a:{b:{c:3}}});
+    rootobj = {a:{b:{c:3}}},
+    Traversal = getMemoryTraversal(rootobj);
 
 
 // testing the mock traversal
@@ -45,4 +46,85 @@ describe('Simple obj Traversal', function(){
 
 });
 
+describe('Getting the original object', function(){
+
+    it('Before traversing', function(){
+      var t = new Traversal();
+      t.get(function (err, obj){
+          obj.should.be.equal(rootobj);
+      });
+    });
+
+    it('After traversing', function(){
+      var t = new Traversal();
+      
+      t.traverse('a', function (err, obj){
+          var t2 = new Traversal({}, obj);
+          
+          
+          t2.get(function (err, obj){
+          obj.should.be.equal(rootobj.a);
+          });
+      });
+    });
+
+
+});
+
+
+describe('Append an object', function(){
+
+    it('Creates a new object', function(){
+      var t = new Traversal();
+      t.create('new', 'newobject', function (err, id){
+          id.should.be.equal('new');
+          t.traverse('new', function (err, obj){
+              obj.should.be.equal('newobject');
+          });
+      });
+    });
+});
+
+describe('Update an object', function(){
+
+    it('Update a object', function(){
+      var t = new Traversal();
+      t.update({a1:{b1:{c1:3}}}, function (err){
+          t.traverse('a1', function (err, obj){
+              should.exist(obj);
+          });
+
+          t.traverse('a', function (err, obj){
+              err.should.be.ok;
+    //          should.strictEqual(undefined,obj);
+          });
+
+
+      });
+    });
+});
+
+describe('Delete object', function(){
+
+    it('Destroy', function(){
+      var t = new Traversal();
+      t.destroy(function (err){
+          t.get(function (err, obj){
+              err.should.be.equal('undefined');
+          });
+      });
+      
+    });
+});
+
+describe('List', function(){
+
+    it('List works', function(){
+      var t = new Traversal();
+      t.list(function (err, lst){
+          lst.should.be.equal(["a"])
+      });
+      
+    });
+});
 
--- test/test_traversal_util.js
+++ test/test_traversal_util.js
@@ -97,6 +97,8 @@ describe('buildTree', function(){
         });
     });
 
+    // test traverse method
+
 });
 
 
